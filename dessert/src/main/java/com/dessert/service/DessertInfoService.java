347db97e69427835c590fa776a185fcc89f5f23c
hj <hj@DESKTOP-H60JQOI.mshome.net>
2022-12-18 02:04:37 +0900

    코드 리팩토링 및 개략적인 기능 구현

--- dessert/src/main/java/com/dessert/service/DessertInfoService.java
+++ dessert/src/main/java/com/dessert/service/DessertInfoService.java
@@ -2,22 +2,33 @@ package com.dessert.service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.dessert.controller.UserInfoController;
 import com.dessert.model.DessertInfo;
 
 public class DessertInfoService {
+	// SingleTone
+	private static DessertInfoService inst = null;
+	public static DessertInfoService getInst() {
+		if (inst == null) {
+			inst = new DessertInfoService();
+		}
+		return inst;
+	}
+
 	public List<DessertInfo> getDessertInfoList() {
 		List<DessertInfo> desserts = new ArrayList<DessertInfo>();
-		
+
 		// TODO) 서버로부터 모든 디저트 정보 받아서 리스트에 저장하고 반환
-		
+
 		return desserts;
 	}
-	
+
 	public boolean updateIsLike(DessertInfo dessertInfo) {
 		boolean isSuccess = true;
-		
+
 		// TODO ) 서버에 데이터 전달, 좋아요와 전체 좋아요 갱신
-		
+
 		return isSuccess;
 	}
 }
